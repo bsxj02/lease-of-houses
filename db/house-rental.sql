3a053bde734c104faf23c42671ef236a25b632e4
bsxj <bsxj@163.com>
2023-10-01 16:18:28 +0800

    Commit Code

---
+++ db/house-rental.sql
@@ -0,0 +1,15 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80027
+ Source Host           : localhost:3306
+ Source Schema         : house-rental
+
+ Target Server Type    : MySQL
+ Target Server Version : 80027
+ File Encoding         : 65001
+
+ Date: 22/04/2023 20:34:05
+*/
